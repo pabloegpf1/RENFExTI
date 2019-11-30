ce3e499fa9b7bc0a18f4fd69475edfd6da0e7d65
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-11-30 19:06:08 +0100

    Twilio

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+const twilio = require('twilio');
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
 router.get('/', function (req, res) {
   res.render('index')
 })
 
 router.post('/submit', function (req, res) {
-  console.log(req.body.name + " - " + req.body.phone);
   let name = req.body.name;
   let number = req.body.phone;
-  res.render('thanks')
+
+  client.messages
+    .create({
+      from: 'whatsapp:' + process.env.ORIGIN_PHONE,
+      to: 'whatsapp:+34' + number,
+      body: name,
+      mediaUrl: 'https://bit.ly/whatsapp-image-example',
+    })
+    .then(message => {
+      console.log(message.sid);
+      res.render('thanks')
+    })
+    .catch(err => {
+      console.error(err);
+      res.render('thanks')
+    });
+
 })
 
 module.exports = router;
