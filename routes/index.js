61d6346ffdb025c3fdce57e7ed173982aabdd6b0
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-11-30 18:35:57 +0100

    Form

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: "Renfe X Tí",
-  })
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+router.post('/submit', function (req, res) {
+  console.log(req.body.name + " - " + req.body.phone);
+  let name = req.body.name;
+  let number = req.body.phone;
+  res.render('thanks')
 })
 
 module.exports = router;
