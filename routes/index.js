4430d31f18d9f60455351e8388e8c0fa3f2ddf2f
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-12-01 11:24:24 +0100

    a

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const twilio = require('twilio');
-const client = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
-
 router.get('/', function (req, res) {
   res.render('index')
 })
